c5f24a72cbc4ee9bbc773c7fdd00c65c82bd7a82
khoffy <khoffy@khoffy>
2023-02-06 14:58:17 -0600

    Project initialization

---
+++ avatar-service/src/main/java/com/sagroup/avatarservice/AvatarServiceApplication.java
@@ -0,0 +1,42 @@
+package com.sagroup.avatarservice;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+
+import com.sagroup.avatarservice.config.ConfigFileExternalizationConfig;
+
+@SpringBootApplication
+public class AvatarServiceApplication {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(AvatarServiceApplication.class);
+
+	public static void main(String[] args) {
+		if (ConfigFileExternalizationConfig.createConfigFiles()
+				!= ConfigFileExternalizationConfig.CONFIG_FILES_ERROR_STATE) {
+			ConfigurableApplicationContext app = new SpringApplicationBuilder(
+				AvatarServiceApplication.class)
+					.build().run(ConfigFileExternalizationConfig.enhanceArgs(args));
+			Environment env = app.getEnvironment();
+			String protocol = "http";
+			if (env.getProperty("server.ssl.key-store") != null) {
+				protocol = "https";
+			}
+			LOGGER.info("\n----------------------------------------------------------\n\t"
+							+ "Application '{}' is running! Access URLs:\n\t"
+							+ "Local: \t\t{}://localhost:{}\n\t"
+							+ "Profile(s): \t{}\n----------------------------------------------------------",
+					env.getProperty("spring.application.name"),
+					protocol,
+					env.getProperty("server.port"),
+					env.getActiveProfiles());
+		} else {
+			LOGGER.error("Please proceed to manually creation of configuration files!");
+		}
+	}
+
+}
