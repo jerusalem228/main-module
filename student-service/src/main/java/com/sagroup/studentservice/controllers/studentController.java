8df8a08997fdb992b65f5e713ce7ffba265056b1
Naod <naodmengis1@gmail.com>
2023-02-09 20:45:07 -0600

    Registration and various different services configuration

--- student-service/src/main/java/com/sagroup/studentservice/controllers/studentController.java
+++ student-service/src/main/java/com/sagroup/studentservice/controllers/studentController.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("api/student")
+@RequestMapping("/api/student")
 public class studentController {
     @Autowired
     private StudentService studentService;
